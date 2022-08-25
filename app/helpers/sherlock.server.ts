dc6593dfe89a827926fee9f2848be06fbfc1c9d7
Jack Warren <jack@jackwarren.info>
2022-08-25 11:19:53 -0400

    helper tweaks

--- app/helpers/sherlock.server.ts
+++ app/helpers/sherlock.server.ts
@@ -1,4 +1,4 @@
-import { Configuration, InitOverrideFunction, ResponseError } from "@sherlock-js-client/sherlock";
+import { Configuration, ResponseError } from "@sherlock-js-client/sherlock";
 
 export const SherlockConfiguration = new Configuration({
     basePath: process.env.SHERLOCK_URL || "http://localhost:8080"
@@ -12,6 +12,9 @@ export function forwardIAP(request: Request): RequestInit {
     }
 }
 
+// Remix lets us throw both Errors and Responses. Responses go to the nearest catch boundary,
+// while Errors go to the nearest error boundary. Here, we open up the reason a promise is
+// about to fail, and throw a Response instead if there is one to trigger the right boundary.
 export function throwErrorResponses(reason: any) {
     if (reason instanceof ResponseError) {
         throw reason.response
