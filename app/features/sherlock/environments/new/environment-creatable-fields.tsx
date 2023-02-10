6b3cbe6d47e18e74bffccbf8c132aa7b292dc2d4
Jack Warren <jack@jackwarren.info>
2023-02-10 16:32:48 -0500

    better cuz why not

--- app/features/sherlock/environments/new/environment-creatable-fields.tsx
+++ app/features/sherlock/environments/new/environment-creatable-fields.tsx
@@ -163,7 +163,7 @@ export const EnvironmentCreatableFields: React.FunctionComponent<
             }
             required={lifecycle !== "dynamic"}
             value={name}
-            pattern="[^A-Z]*"
+            pattern="[a-z0-9]([-a-z0-9]*[a-z0-9])?"
             onChange={(e) => setName(e.currentTarget.value)}
           />
         </label>
