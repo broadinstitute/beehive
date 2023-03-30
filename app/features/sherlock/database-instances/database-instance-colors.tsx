2592c45124523f7a44f057348d34d946ee031ef5
Jack Warren <jack@jackwarren.info>
2023-03-30 18:12:58 -0400

    databaseinstance feature

---
+++ app/features/sherlock/database-instances/database-instance-colors.tsx
@@ -0,0 +1,4 @@
+import { ColorProps } from "../../color-class-names";
+import { ChartReleaseColors } from "../chart-releases/chart-release-colors";
+
+export const DatabaseInstanceColors: ColorProps = ChartReleaseColors;
