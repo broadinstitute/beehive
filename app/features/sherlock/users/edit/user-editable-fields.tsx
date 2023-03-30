cfcb3fdfe05a8b5de5790053dd3dd2abe0c5bc8e
Jack Warren <jack@jackwarren.info>
2023-03-30 17:09:39 -0400

    improvements to the user edit screen

--- app/features/sherlock/users/edit/user-editable-fields.tsx
+++ app/features/sherlock/users/edit/user-editable-fields.tsx
@@ -15,25 +15,25 @@ export const UserEditableFields: React.FunctionComponent<{
   );
   return (
     <div className="flex flex-col gap-4">
-      <div>
+      <div className="flex flex-col gap-2">
         <h2 className="font-light text-2xl">Get Name From GitHub Profile?</h2>
         <p>
           Our platform tries to keep track of everyone's names, nicknames, or
           usernames to help identify you to other folks in Beehive.
         </p>
-        <p className="mt-2">
+        <p>
           This field isn't intentionally shown publicly outside of our platform,
           but it isn't considered sensitive by our systems either, so it could
           potentially get logged.
         </p>
-        <p className="mt-2">
+        <p>
           By default, our systems will keep your name here consistent with the
           name on your GitHub profile. You can change that behavior here if
           you'd like.
         </p>
         <EnumInputSelect
           name="nameInferredFromGithub"
-          className="grid grid-cols-2 mt-2"
+          className="grid grid-cols-2"
           fieldValue={nameInferredFromGithub}
           setFieldValue={setNameInferredFromGithub}
           enums={[
@@ -64,6 +64,24 @@ export const UserEditableFields: React.FunctionComponent<{
             <TextField name="name" defaultValue={user?.name} />
           </label>
         )}
+      </div>
+      <div className="flex flex-col gap-2">
+        <h2 className="font-light text-2xl">Changing Other Information</h2>
+        <p>
+          If you'd like to change your GitHub login username, you can do that
+          through GitHub. Once that's done, you can click save here and Beehive
+          will pick up any changes.
+        </p>
+        <p>
+          If you'd like to change your Broad Institute email address, that's a
+          bit more complicated. BITS can add an alias to your email account with
+          relative ease, but the original address will still be there (maybe
+          that's desirable behavior, maybe it isn't). If you want your actual
+          email address itself changed, that's unfortunately a bit of a lift.
+          You'd need to contact BITS and DevOps and we'd need to coordinate a
+          number of manual changes to keep everything wired up for your
+          permissions and access.
+        </p>
       </div>
     </div>
   );
