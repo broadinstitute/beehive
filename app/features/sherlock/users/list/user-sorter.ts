85f685cb7d3955caf9af26869a2d8ab1bd1d921f
Jack Warren <jack@jackwarren.info>
2023-03-31 17:51:02 -0400

    [DDO-2759] show user list for environment owner

--- app/features/sherlock/users/list/user-sorter.ts
+++ app/features/sherlock/users/list/user-sorter.ts
@@ -1,7 +1,7 @@
 import { V2controllersUser } from "@sherlock-js-client/sherlock";
 
 export function makeUserSorter(
-  emailToComeFirst?: string
+  emailToComeFirst?: string | null
 ): (a: V2controllersUser, b: V2controllersUser) => number {
   return (a, b) => {
     if (a.email === emailToComeFirst) {
