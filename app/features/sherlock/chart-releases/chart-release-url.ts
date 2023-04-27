eb7e20cda6c60bfc206361d3a175a5048d91525f
Jack Warren <jack@jackwarren.info>
2023-04-27 17:50:58 -0400

    fn to calc urls

---
+++ app/features/sherlock/chart-releases/chart-release-url.ts
@@ -0,0 +1,38 @@
+import { SerializeFrom } from "@remix-run/node";
+import { V2controllersChartRelease } from "@sherlock-js-client/sherlock";
+
+export function chartReleaseUrl(
+  chartRelease:
+    | V2controllersChartRelease
+    | SerializeFrom<V2controllersChartRelease>
+    | null
+): string | null {
+  if (
+    !chartRelease ||
+    !chartRelease.environment ||
+    !chartRelease.chartInfo?.chartExposesEndpoint
+  ) {
+    return null;
+  }
+
+  const protocol =
+    chartRelease.protocol || chartRelease.chartInfo?.defaultProtocol || "https";
+  let url = protocol;
+  url += "://";
+  url +=
+    chartRelease.subdomain ||
+    chartRelease.chartInfo?.defaultSubdomain ||
+    chartRelease.chart;
+  url += ".";
+  if (chartRelease.environmentInfo?.namePrefixesDomain) {
+    url += chartRelease.environment;
+    url += ".";
+  }
+  url += chartRelease.environmentInfo?.baseDomain;
+  const port = chartRelease.port || chartRelease.chartInfo.defaultPort || 443;
+  if (!(protocol === "https" && port === 443)) {
+    url += ":";
+    url += port;
+  }
+  return url;
+}
