e096fac1f47fec98268cc58ad8d0dcad70e6c048
Jack Warren <jack@jackwarren.info>
2023-08-30 13:10:17 -0400

    update header

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -38,8 +38,8 @@ export default function Route() {
           Welcome to Beehive
         </h1>
         <span className="text-lg text-center">
-          <b className="font-semibold">New!</b> View GitHub Actions workflow
-          runs from Beehive!
+          <b className="font-semibold">New!</b> Run GitHub Actions and send
+          Slack notifications upon deployments with Deploy Hooks!
         </span>
       </div>
       <div className="flex flex-wrap justify-center w-full max-w-7xl m-auto min-h-0 items-center">
