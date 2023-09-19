cdf501203fe17169913ec4d4775ee41215804159
Eno Akpan <eno.abasi@icloud.com>
2023-09-19 10:15:54 -0400

    Add env

--- setup-env.bash
+++ setup-env.bash
@@ -3,6 +3,11 @@
 vault read -format json secret/suitable/beehive/local/github-oauth | 
   jq -r  '.data | to_entries[] | "GITHUB_\(.key | ascii_upcase)=\(.value)"' > .env
 
+# Pact credentials & configurations
+echo "PACTPASSWORD=$(vault read -format json 'secret/dsp/pact-broker/users/readwrite' | jq -r .data.basic_auth_password)" >> .env
+echo "PACTUSERNAME=$(vault read -format json 'secret/dsp/pact-broker/users/readwrite' | jq -r .data.basic_auth_username)" >> .env
+echo "PACTBASEURL=https://pact-broker.dsp-eng-tools.broadinstitute.org" >> .env
+
 echo "COOKIE_SIGNING_SECRET=$(echo $RANDOM | md5sum | head -c 20)" >> .env
 
 echo "PAGERDUTY_APP_ID=P2DQ0G3" >> .env
