5accb1bfc17aad70e43e27537e5ef90b76715889
Jack Warren <jack@jackwarren.info>
2023-10-19 15:09:25 -0400

    tweak text (#359)

--- app/features/sherlock/pagerduty-integrations/trigger-incident/incident-trigger-success-panel.tsx
+++ app/features/sherlock/pagerduty-integrations/trigger-incident/incident-trigger-success-panel.tsx
@@ -1,6 +1,6 @@
 import { OutsetPanel } from "~/components/layout/outset-panel";
 import { ItemDetails } from "~/components/panel-structures/item-details";
-import { ColorProps } from "~/features/color-class-names";
+import type { ColorProps } from "~/features/color-class-names";
 
 export const IncidentTriggerSuccessPanel: React.FunctionComponent<
   Partial<ColorProps>
@@ -8,12 +8,13 @@ export const IncidentTriggerSuccessPanel: React.FunctionComponent<
   <OutsetPanel {...props}>
     <ItemDetails title="Incident Triggered">
       <p>
-        An incident has been triggered. You can follow-up with the on-call
-        engineer in{" "}
+        An incident has been triggered. To follow up with the on-call engineer,
+        start a conversation in{" "}
         <a
           href="https://broadinstitute.slack.com/archives/CA1481PRN"
           target="_blank"
           className="underline decoration-color-link-underline"
+          rel="noreferrer"
         >
           #workbench-resilience in Slack
         </a>
