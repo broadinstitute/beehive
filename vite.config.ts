a3977a2ef450ea43d44776f0c61db11026443ff6
Jack Warren <jack@jackwarren.info>
2024-08-24 17:58:24 -0400

    no globals!

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import mdx from "@mdx-js/rollup";
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-installGlobals();
 
 export default defineConfig({
   server: {
